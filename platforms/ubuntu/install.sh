7b04dc83ccf2dd30a7a6e86b616abd7b9a849500
Eric McNiece <emcniece@gmail.com>
2017-02-24 16:10:50 -0800

    Serializing the crap out of everything

--- platforms/ubuntu/install.sh
+++ platforms/ubuntu/install.sh
@@ -6,22 +6,22 @@ echo ""
 sudo apt-get update && sudo apt-get install -y docker.io python-pip
 
 # Rancher-Compose
-echo "Installing Rancher-Compose..."
-echo ""
-RCDL=$(curl -s https://api.github.com/repos/rancher/rancher-compose/releases/latest \
+&& echo "Installing Rancher-Compose..."
+&& echo ""
+&& RCDL=$(curl -s https://api.github.com/repos/rancher/rancher-compose/releases/latest \
   | grep browser_download_url \
   | grep linux-amd64 \
   | grep tar.gz \
   | head -n 1 \
   | cut -d '"' -f 4)
 
-wget -qO- $RCDL | sudo tar xz -C /tmp
-sudo find /tmp -name rancher-compose -exec mv -t /usr/bin {} +
+&& wget -qO- $RCDL | sudo tar xz -C /tmp
+&& sudo find /tmp -name rancher-compose -exec mv -t /usr/bin {} +
 
 # Bash Aliases
-echo "Adding Aliases..."
-echo ""
-cat >~/.profile <<EOL
+&& echo "Adding Aliases..."
+&& echo ""
+&& cat >~/.profile <<EOL
 alias dps='docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}\t{{.Names}}"'
 alias dlog='docker logs -f'
 
@@ -39,25 +39,25 @@ export RANCHER_SECRET_KEY=[SECRET_FROM_RANCHER_API]
 EOL
 
 # Rancher
-echo "Pulling Rancher..."
-echo ""
-docker pull rancher/server
-docker pull rancher/agent
+&& echo "Pulling Rancher..."
+&& echo ""
+&& docker pull rancher/server
+&& docker pull rancher/agent
 
 # Docker-Compose
-echo "Installing Docker-Compose..."
-echo ""
-sudo usermod -aG docker $(whoami)
-exec su -l $USER
-sudo pip install docker-compose
+&& echo "Installing Docker-Compose..."
+&& echo ""
+&& sudo usermod -aG docker $(whoami)
+&& exec su -l $USER
+&& sudo pip install docker-compose
 
 # Summary
-echo "-- INSTALL COMPLETE --"
-echo ""
-echo "1. Run Rancher: "
-echo "docker run -d --name=rancher -p 8080:8080 rancher/server"
-echo ""
-echo "2. Generate API credentials for Rancher-Compose"
-echo "http://localhost:8080/env/1a5/api/keys"
-echo ""
-echo "3. Add API keys to ~/.profile"
+&& echo "-- INSTALL COMPLETE --"
+&& echo ""
+&& echo "1. Run Rancher: "
+&& echo "docker run -d --name=rancher -p 8080:8080 rancher/server"
+&& echo ""
+&& echo "2. Generate API credentials for Rancher-Compose"
+&& echo "http://localhost:8080/env/1a5/api/keys"
+&& echo ""
+&& echo "3. Add API keys to ~/.profile"
