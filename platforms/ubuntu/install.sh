72098cea5f4af62a39597eadc1909bb28818155b
Eric McNiece <emcniece@gmail.com>
2017-02-24 16:53:05 -0800

    Rearranging order

--- platforms/ubuntu/install.sh
+++ platforms/ubuntu/install.sh
@@ -3,25 +3,24 @@
 echo "Installing Docker..." && echo "" &&
 sudo apt-get update &&
 sudo apt-get install -y docker.io python-pip &&
+echo "Installing Docker-Compose..." && echo "" &&
+sudo usermod -aG docker $(whoami) &&
+sudo pip install docker-compose &&
 echo "Installing Rancher-Compose..." && echo "" &&
 RCDL=$(curl -s https://api.github.com/repos/rancher/rancher-compose/releases/latest | grep browser_download_url | grep linux-amd64 | grep tar.gz | head -n 1 | cut -d '"' -f 4) &&
 wget -qO- $RCDL | sudo tar xz -C /tmp &&
 sudo find /tmp -name rancher-compose -exec mv -t /usr/bin {} + &&
 echo "Adding Aliases..." && echo "" &&
 curl https://raw.githubusercontent.com/emcniece/docker-rancher-install/master/aliases.txt >> ~/.profile &&
-echo "Pulling Rancher..." && echo "" &&
-docker pull rancher/server &&
-docker pull rancher/agent &&
-echo "Installing Docker-Compose..." && echo "" &&
-sudo usermod -aG docker $(whoami) &&
-exec su -l $USER &&
-sudo pip install docker-compose &&
 echo "-- INSTALL COMPLETE --" &&
 echo "" &&
-echo "1. Run Rancher: " &&
+echo "1. Restart this terminal session" &&
+echo "" &&
+echo "2. Run Rancher: " &&
+echo "docker pull rancher/server" &&
 echo "docker run -d --name=rancher -p 8080:8080 rancher/server" &&
 echo "" &&
-echo "2. Generate API credentials for Rancher-Compose" &&
+echo "3. Generate API credentials for Rancher-Compose" &&
 echo "http://localhost:8080/env/1a5/api/keys" &&
 echo "" &&
-echo "3. Add API keys to ~/.profile"
+echo "4. Add API keys to ~/.profile"
